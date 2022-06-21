32d01b42e9bdb4c1262af20d0c67b196b9734496
robby oktahidayat <robbyoktahidayat@gmail.com>
2022-06-21 11:01:58 +0700

    challenge Done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,19 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = (props) => {
+  console.log(props);
+  return (
+    <div className="card" myContact="card">
+        <img className="foto" myContact="photo" src={props.data.photo} alt="foto"></img>
+      <div className="data">
+        <h3>{props.data.name}</h3>
+        <p>{props.data.phone}</p>
+        <p>{props.data.email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
